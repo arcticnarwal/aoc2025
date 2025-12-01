1535609d9fc3b97398b46413744cfc88513c84ea
Jason Bogh <arcticnarwal@gmail.com>
2025-12-01 10:08:22 -0600

    Completed Day01

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
-import Day01.Day01;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Day01.getSolution());
+        // System.out.println(Day01.Day01.getSolution());
+        System.out.println(Day01.Day01p2.getSolution());
     }
 }
--- src/Day01/Day01p2.java
+++ src/Day01/Day01p2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Day01 {
+public class Day01p2 {
     public static int getSolution() {
         String filePath = "src/Day01/input.txt";
         char subtract = 'L';
@@ -20,26 +20,31 @@ public class Day01 {
                 int rotations = Integer.parseInt(line.substring(1));
 
                 if (rotations > maxPoint) {
+                    zeroHitsCount += rotations / (maxPoint + 1);
                     rotations = rotations % (maxPoint + 1);
                 }
                 if (direction == subtract) {
                     if (rotations > currentPoint) {
+                        if (currentPoint != 0) {
+                            zeroHitsCount++;
+                        }
                         currentPoint = (maxPoint + 1) - (rotations - currentPoint);
                     }
                     else {
                         currentPoint = currentPoint - rotations;
                     }
+                    if (currentPoint == 0) {
+                        zeroHitsCount++;
+                    }
                 }
                 else if (direction == add) {
                     if (rotations > maxPoint - currentPoint) {
+                        zeroHitsCount++;
                         currentPoint = rotations - (maxPoint - currentPoint) - 1;
                     }
                     else {
                         currentPoint = currentPoint + rotations;
                     }
-                }
-                if (currentPoint == 0) {
-                    zeroHitsCount++;
                 }
             }   
         } catch (IOException e) {
@@ -111,4 +116,32 @@ The dial is rotated L82 to point at 32.
 Because the dial points at 0 a total of three times during this process, the password in this example is 3.
 
 Analyze the rotations in your attached document. What's the actual password to open the door?
+
+--- Part Two ---
+You're sure that's the right password, but the door won't open. You knock, but nobody answers. You build a snowman while you think.
+
+As you're rolling the snowballs for your snowman, you find another security document that must have fallen into the snow:
+
+"Due to newer security protocols, please use password method 0x434C49434B until further notice."
+
+You remember from the training seminar that "method 0x434C49434B" means you're actually supposed to count the number of times any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+
+Following the same rotations as in the above example, the dial points at zero a few extra times during its rotations:
+
+The dial starts by pointing at 50.
+The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+The dial is rotated L30 to point at 52.
+The dial is rotated R48 to point at 0.
+The dial is rotated L5 to point at 95.
+The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+The dial is rotated L55 to point at 0.
+The dial is rotated L1 to point at 99.
+The dial is rotated L99 to point at 0.
+The dial is rotated R14 to point at 14.
+The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+In this example, the dial points at 0 three times at the end of a rotation, plus three more times during a rotation. So, in this example, the new password would be 6.
+
+Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial to point at 0 ten times before returning back to 50!
+
+Using password method 0x434C49434B, what is the password to open the door?
  */
